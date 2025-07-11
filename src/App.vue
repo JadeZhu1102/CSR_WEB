71a3c5c3902fbf90db35924673172dfd9638fbe1
ZhuYu <1104268597@qq.com>
2025-07-11 21:20:37 +0800

    page title

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@ import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
 import { checkIfLogined } from "@/util/auth";
 import { initializeLocale } from "@/config/locale";
 import PageUrl from "@/config/page-url";
+import { useI18n } from 'vue-i18n';
+
+// 页面路径与i18n key映射
+const pageTitleMap: Record<string, string> = {
+  'pages/index/index': 'title.all_activities',
+  'pages/activity/detail': 'title.activity_detail',
+  'pages/activity/auto-join': 'title.join_activity',
+  'pages/account/index': 'title.my_account',
+  'pages/auth/login': 'title.login',
+  'pages/auth/register': 'title.register',
+  // ...如有其它页面请补充
+};
+
+const { t } = useI18n();
 
 onLaunch(() => {
   initializeLocale();
@@ -17,7 +31,17 @@ onLaunch(() => {
 });
 
 onShow(() => {
-  console.log("App Show");
+  const pages = getCurrentPages();
+  const currentPage = pages[pages.length - 1];
+  // @ts-ignore
+  const route = currentPage?.route;
+  if (route && pageTitleMap[route]) {
+    const title = t(pageTitleMap[route]);
+    uni.setNavigationBarTitle({ title });
+    if (typeof document !== 'undefined') {
+      document.title = title;
+    }
+  }
 });
 onHide(() => {
   console.log("App Hide");
